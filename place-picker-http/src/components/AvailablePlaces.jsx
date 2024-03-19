71c5720c6eb32a633397309239696f0e8fee50ee
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-19 10:48:13 +0200

    add a text for loading

--- place-picker-http/src/components/AvailablePlaces.jsx
+++ place-picker-http/src/components/AvailablePlaces.jsx
@@ -3,12 +3,15 @@ import Places from "./Places.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
+  const [isFetching, setIsFetching] = useState(false);
 
   useEffect(() => {
+    setIsFetching(true);
     async function fetchPlaces() {
       const response = await fetch("http://localhost:3000/places");
       const resData = await response.json();
       setAvailablePlaces(resData.places);
+      setIsFetching(false);
     }
     fetchPlaces();
   }, []);
@@ -17,6 +20,8 @@ export default function AvailablePlaces({ onSelectPlace }) {
     <Places
       title="Available Places"
       places={availablePlaces}
+      isLoading={isFetching}
+      loadingText="Fetching data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
