c23ced2f37122faf11405dad2a150b67773e22be
ioana.girba@principal33.com <ioana.girba@ITRO-166.local>
2024-03-19 11:05:33 +0200

    add error message for UI

--- place-picker-http/src/components/AvailablePlaces.jsx
+++ place-picker-http/src/components/AvailablePlaces.jsx
@@ -1,20 +1,38 @@
 import { useEffect, useState } from "react";
 import Places from "./Places.jsx";
+import Error from "./Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [availablePlaces, setAvailablePlaces] = useState([]);
   const [isFetching, setIsFetching] = useState(false);
+  const [error, setError] = useState();
 
   useEffect(() => {
-    setIsFetching(true);
     async function fetchPlaces() {
-      const response = await fetch("http://localhost:3000/places");
-      const resData = await response.json();
-      setAvailablePlaces(resData.places);
+      setIsFetching(true);
+
+      try {
+        const response = await fetch("http://localhost:3000/places");
+        const resData = await response.json();
+
+        if (!response.ok) {
+          throw new Error("Failed to fetch places");
+        }
+        setAvailablePlaces(resData.places);
+      } catch (error) {
+        setError({
+          message: error.message || "Could not fetch, try again later",
+        });
+      }
+
       setIsFetching(false);
     }
     fetchPlaces();
   }, []);
+
+  if (error) {
+    return <Error title="An error ocurred!" message={error.message} />;
+  }
 
   return (
     <Places
